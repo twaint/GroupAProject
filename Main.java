6264866f625629ee1a71fa123357b3f53541df3b
BelliJoseph <70913604+BelliJoseph@users.noreply.github.com>
2021-08-08 20:59:52 -0400

    Main menu class

---
+++ Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupa.ceis400courseProject;
+
+/**
+ *
+ * @author joebe
+ */
+public interface Main {
+    
+    //Methods
+    public void uiMenu(){}
+    
+    public void uiLogIn(){}
+    
+    public void uiCheckInEquipment(){}
+    
+    public void uiCheckOutEquipment(){}
+    
+    public void uiReportGenerator(){}
+    
+    public void uiEmployeeManagement(){}
+    
+    public void uiWarehouseOrder(){}
+    
+    public void uiLogOut(){}
+}
