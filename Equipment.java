3e5727d03e04de43ed4a6e1f69b1a8da4d81b644
BelliJoseph <70913604+BelliJoseph@users.noreply.github.com>
2021-08-12 17:17:01 -0400

    Added method details along with constructors.
    May need to update once the coding on the other classes progresses.

--- Equipment.java
+++ Equipment.java
@@ -12,8 +12,8 @@ package com.groupa.ceis400courseProject;
 public class Equipment {
     
     //Declare Variables
-    private String equipmentName;
     private int equipmentNumber;
+    private String equipmentName;
     private String description;
     private boolean equipmentDamaged;
     private String buildingLocation;
@@ -22,21 +22,93 @@ public class Equipment {
     private int borrowerID;
     
     //Methods
-    public Equipment(){}
-    public void setEquipmentName(String toolName){}
-    public String getEquipmentName(){}
-    public void setEquipmentNumber(int toolNum){}
-    public int getEquipmentNumber(){}
-    public void setDescription(String description){}
-    public String getDescription(){}
-    public void setEquipmentDamaged(boolean toolDamage){}
-    public boolean getEquipmentDamaged(){}
-    public void setBuildingLocation(String buildLoc){}
-    public String getBuildingLocation(){}
-    public void setShelfLocation(String shelfLoc){}
-    public String getShelfLocation(){}
-    public void setIsAssigned(boolean assign){}
-    public boolean getIsAssigned(){}
-    public void setBorrower(int borrowID){}
-    public int getBorrower(){}
+    public Equipment(){
+        this.equipmentNumber = 0;
+        this.equipmentName = null;
+        this.description = null;
+        this.equipmentDamaged = false;
+        this.buildingLocation = null;
+        this.shelfLocation = null;
+        this.isAssigned = false;
+        this.borrowerID = 0;
+    }
+    
+    
+    public Equipment(int equipmentNumber, String equipmentName, String description,
+            boolean equipmentDamaged, String buildingLocation, String shelfLocation,
+            boolean isAssigned, int borrowerID){
+        
+        this.equipmentNumber = equipmentNumber;
+        this.equipmentName = equipmentName;
+        this.description = description;
+        this.equipmentDamaged = equipmentDamaged;
+        this.buildingLocation = buildingLocation;
+        this.shelfLocation = shelfLocation;
+        this.isAssigned = isAssigned;
+        this.borrowerID = borrowerID;
+    }
+    
+    public void setEquipmentNumber(int equipmentNumber){
+        this.equipmentNumber = equipmentNumber;
+    }
+    
+    public int getEquipmentNumber(){
+        return equipmentNumber;
+    }
+    
+    public void setEquipmentName(String equipmentName){
+        this.equipmentName = equipmentName;
+    }
+    
+    public String getEquipmentName(){
+        return equipmentName;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public void setEquipmentDamaged(boolean equipmentDamaged){
+        this.equipmentDamaged = equipmentDamaged;
+    }
+    
+    public boolean getEquipmentDamaged(){
+        return equipmentDamaged;
+    }
+    
+    public void setBuildingLocation(String buildingLocation){
+        this.buildingLocation = buildingLocation;
+    }
+    
+    public String getBuildingLocation(){
+        return buildingLocation;
+    }
+    
+    public void setShelfLocation(String shelfLocation){
+        this.shelfLocation = shelfLocation;
+    }
+    
+    public String getShelfLocation(){
+        return shelfLocation;
+    }
+    
+    public void setIsAssigned(boolean isAssigned){
+        this.isAssigned = isAssigned;
+    }
+    
+    public boolean getIsAssigned(){
+        return isAssigned;
+    }
+    
+    public void setBorrowerID(int borrowerID){
+        this.borrowerID = borrowerID;
+    }
+    
+    public int getBorrowerID(){
+        return borrowerID;
+    }
 }
